266f2a721af6ba007372a46cafea82f234960f6b
Sheila Kobue <maserole.mojela@gmail.com>
2023-07-04 15:30:14 +0200

    added new files

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-/* Surprise! You probably thought you could just forget
-the line of code you just learned! Nope, not on my watch!
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-Try to write that 1-liner of React code again! This time,
-see if you can figure out how to render an <ul> with 2+ <li>s inside*/
-ReactDOM.render(
-    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
-    document.getElementById("root")
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
+/**
+ * 
+ */
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(navbar)
+
+ReactDOM.createRoot(document.getElementById("root")).render(navbar)
