a319f5c1627aa1ea60c7035026a7d2ff3d7f2f8f
Sheila Kobue <maserole.mojela@gmail.com>
2023-07-05 11:36:15 +0200

    deleted files

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ function WhyReact() {
             <img src="./react-logo.png" width="40px" />
             <h1>Reasons why i learn react</h1>
             <ol>
-                <li>Popularity</li>
-                <li>Reusability</li>
-                <li>Virtual DOM</li>
-                <li>One-Way Data Flow</li>
-                <li>Large and Active Community</li>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has maintained by Facebook</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ol>
         </div>
     )
 }
 
 ReactDOM.render(<WhyReact />, document.getElementById("root"))
+ReactDOM.createRoot(document.getElementById("root")).render(navbar)
+
