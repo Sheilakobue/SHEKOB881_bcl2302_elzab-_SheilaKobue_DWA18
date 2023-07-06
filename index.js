b591f1362ec78999e7c9de3b7ae92605e58ff03e
Sheila Kobue <maserole.mojela@gmail.com>
2023-07-06 11:39:22 +0200

    created new files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom"
+//import React from "react"
+//import ReactDOM from "react-dom"
 import Footer from "./Footer"
 import Header from "./Header"
 import MainContent from "./MainContent"
