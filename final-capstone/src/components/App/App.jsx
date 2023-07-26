21e4b49faa65dc47a2834a1a1e3d6190869d2212
Sheila Kobue <maserole.mojela@gmail.com>
2023-07-26 09:41:43 +0200

    moved file and styled my navbar

--- final-capstone/src/components/App/App.jsx
+++ final-capstone/src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 //import Card from './components/Card'
-import ShowMore from './components/ShowMore'
-import NavBar from './components/NavBar'
+import ShowMore from '../ShowMore'
+import NavBar from '../NavBar'
 //import { Sidebar } from 'react-pro-sidebar'
 
 
--- final-capstone/src/data/CardData.jsx
+++ final-capstone/src/data/CardData.jsx
@@ -1,52 +1,32 @@
-import {useState} from 'react'
-import Grid from '@mui/material/Grid'
+import React, { useState, useEffect } from 'react';
+import Card from './Card'; // Importing the Card component
+import { Grid } from '@mui/material';
 
+export default function CardData() {
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    fetch("https://podcast-api.netlify.app/shows")
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      })
+      .catch((error) => console.error('Error fetching previews:', error));
+  }, []);
 
-export default function CardData(){
-    const [count, setCount] = useState(null)
-
-function Card(){
-        fetch("https://podcast-api.netlify.app/shows")
-        .then(response=>response.json())
-        .then(data =>{
-            console.log(data)
-  
-            const mapData = data.map((dataMapping)=>{
-                return (<Card
-                    key={dataMapping.id}
-                    titles = {dataMapping.title}
-                    descriptions = {dataMapping.description}
-                    seasons = {dataMapping.season}
-                    images = {dataMapping.image}
-
-                    />)
-            })
-            setCount(mapData)
-        })
-    }
-    return(
-        <>
-                 
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>{count} </Grid>
-        {Card()} 
-        </>
-    )
-}
-
-import React from "react"
-import CardData from "../data/CardData"
-import {Grid} from '@mui/material'
-
-export default function Card(props){
-
-    return (
-        <Grid item xs={3}>
-        <p>{props.id}</p>
-        <h3>{props.titles}</h3>
-        <h4>{props.descriptions.substring(0,150)}</h4>
-        <h4>{props.seasons}</h4>
-        <img className="card--image" src={props.images}></img>
+  return (
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+      {data.map((dataMapping) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={dataMapping.id}>
+          <Card
+            id={dataMapping.id}
+            titles={dataMapping.title}
+            descriptions={dataMapping.description}
+            seasons={dataMapping.season}
+            images={dataMapping.image}
+          />
         </Grid>
-    )
+      ))}
+    </Grid>
+  );
 }
