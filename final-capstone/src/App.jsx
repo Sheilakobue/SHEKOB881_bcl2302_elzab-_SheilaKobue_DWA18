29e1fe704284b05e91286c5c901b3b67bf21f7c6
Sheila Kobue <maserole.mojela@gmail.com>
2023-07-28 20:49:19 +0200

    created new files

---
+++ final-capstone/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import {Header} from './'
+import './App.css'
+
+function App() {
+  
+      return(
+        <div>
+          <Header/>
+            
+        </div>
+        
+    )
+}
+
+export default App
--- final-capstone/src/main.jsx
+++ final-capstone/src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './components/App/App'
-import {BrowserRouter} from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render( 
-    <React.StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-    </BrowserRouter>
-    </React.StrictMode>
-    
-    )
+  </React.StrictMode>,
+)
